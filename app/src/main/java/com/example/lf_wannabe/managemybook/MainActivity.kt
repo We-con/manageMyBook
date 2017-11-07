6337e2fa7948c5aaee66fb62331e9d49ccad8226
GYUJEONG HWANG <octave08b@gmail.com>
2017-11-07 15:42:48 +0900

    Fix Toolbar
    Toolbar를 수정했습니다.
    - 높이를 46에서 48로 수정 
    - toolbar의 id 수정
    - 자잘한 UI 수정

--- app/src/main/java/com/example/lf_wannabe/managemybook/MainActivity.kt
+++ app/src/main/java/com/example/lf_wannabe/managemybook/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lf_wannabe.managemybook
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.lf_wannabe.managemybook.view.AddBookActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,10 +13,17 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setTitle("섹스")
+        // Toolbar 설정
+        setTitle("문학소년")
+        setAction(View.OnClickListener { showMessage() })
 
+        // Fab 설정
         mainFab.setOnClickListener {
             startActivity(AddBookActivity::class.java)
         }
+    }
+
+    private fun showMessage(message: String = "Comming soon") {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
