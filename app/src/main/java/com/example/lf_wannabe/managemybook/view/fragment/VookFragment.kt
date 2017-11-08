4562f47e151ec3c9494d1db84cd3a2c418d28bdf
GYUJEONG HWANG <octave08b@gmail.com>
2017-11-08 22:23:27 +0900

    Add author and publisher style
    Vook에서 author와 publiser에 스타일을 추가

--- app/src/main/java/com/example/lf_wannabe/managemybook/view/fragment/VookFragment.kt
+++ app/src/main/java/com/example/lf_wannabe/managemybook/view/fragment/VookFragment.kt
@@ -2,12 +2,16 @@ package com.example.lf_wannabe.managemybook.view.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.lf_wannabe.managemybook.R
 import com.example.lf_wannabe.managemybook.model.Book
+import com.example.lf_wannabe.managemybook.util.TextFormatUtil
 import kotlinx.android.synthetic.main.fragment_vook_item.view.*
 
 /**
@@ -31,14 +35,49 @@ class VookFragment : Fragment() {
     private fun bindData(root: View) {
         with(root) {
             vookTextTitle.text = book.title
-            vookTextDetails.text = book.author
+            vookTextDetails.text = makeDetails(book.author, book.publisher)
             vookTextNowPage.text = "P ${book.currentPageNum}"
             vookTextTotalPage.text = "P ${book.totalPageNum}"
-            vookTextComments.text = getComments(book.currentPageNum, book.totalPageNum)
+            vookTextComments.text = makeComments(book.currentPageNum, book.totalPageNum)
         }
     }
 
-    private fun getComments(current: Int, total: Int): String {
+    private fun makeDetails(author: String, publisher: String): SpannableStringBuilder {
+        var author = SpannableString(author).let {
+            TextFormatUtil.changeSize(it, 15)
+            TextFormatUtil.changeColor(it, ContextCompat.getColor(context, R.color.colorTextBody1))
+        }
+
+        var unitAuthor = SpannableString(" 지음").let {
+            TextFormatUtil.changeSize(it, 12)
+            TextFormatUtil.changeColor(it, ContextCompat.getColor(context, R.color.colorTextBody2))
+        }
+
+        var divider = SpannableString("  |  ").let {
+            TextFormatUtil.changeSize(it, 12)
+            TextFormatUtil.changeColor(it, ContextCompat.getColor(context, R.color.colorTextBody2))
+        }
+
+        var publisher = SpannableString(publisher).let {
+            TextFormatUtil.changeSize(it, 15)
+            TextFormatUtil.changeColor(it, ContextCompat.getColor(context, R.color.colorTextBody1))
+        }
+
+        var unitPublisher = SpannableString(" 펴냄").let {
+            TextFormatUtil.changeSize(it, 12)
+            TextFormatUtil.changeColor(it, ContextCompat.getColor(context, R.color.colorTextBody2))
+        }
+
+        return SpannableStringBuilder().apply {
+            append(author)
+            append(unitAuthor)
+            append(divider)
+            append(publisher)
+            append(unitPublisher)
+        }
+    }
+
+    private fun makeComments(current: Int, total: Int): String {
         var progress = (current*100)/total
         if(progress <= 0) { progress = 0 }
         else if(progress < 100) { progress = progress/10 + 1 }
