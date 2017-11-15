3ddb36377373d97a0c0509699ff3202d287ebf75
Unknown <cms3718@gmail.com>
2017-11-15 12:38:58 +0900

    fix UI of addPostPage
    - add WheelView on Bottom

--- app/src/main/java/com/example/lf_wannabe/managemybook/view/AddPostActivity.kt
+++ app/src/main/java/com/example/lf_wannabe/managemybook/view/AddPostActivity.kt
@@ -3,6 +3,10 @@ package com.example.lf_wannabe.managemybook.view
 import android.os.Bundle
 import android.widget.Toast
 import com.example.lf_wannabe.managemybook.BaseActivity
+import com.example.lf_wannabe.managemybook.R
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_add_post.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by lf_wannabe on 12/11/2017.
@@ -10,10 +14,23 @@ import com.example.lf_wannabe.managemybook.BaseActivity
 class AddPostActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_post)
+
+        initToolbar()
+        initWheelView()
     }
 
 
+    fun initWheelView(){
+        var list: ArrayList<String> = ArrayList()
+        // ㅎㅎ;
+        Observable.range(1, 1000)
+                .map { i -> i.toString() }
+                .subscribe{ i -> list.add(i)}
 
+        addPostWheelView.items = list
+        addPostWheelView.setAdditionCenterMark(".p")
+    }
 
     override fun initToolbar() {
         setTitle("포스트 입력")
