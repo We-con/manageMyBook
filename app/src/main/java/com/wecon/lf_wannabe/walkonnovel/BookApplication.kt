4992a910fff6ae156c0c8c0de64bb586bb89daab
Unknown <cms3718@gmail.com>
2017-11-22 03:31:10 +0900

    apply multidex

--- app/src/main/java/com/wecon/lf_wannabe/walkonnovel/BookApplication.kt
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/BookApplication.kt
@@ -1,6 +1,7 @@
 package com.wecon.lf_wannabe.walkonnovel
 
 import android.app.Application
+import android.support.multidex.MultiDexApplication
 import com.wecon.lf_wannabe.walkonnovel.model.BookDao
 import com.wecon.lf_wannabe.walkonnovel.util.DummyFactory
 import io.realm.Realm
@@ -8,7 +9,7 @@ import io.realm.Realm
 /**
  * Created by lf_wannabe on 06/11/2017.
  */
-class BookApplication : Application() {
+class BookApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(applicationContext)
