00a9e4eeae18152de105aec3a7878499fc9faed4
GYUJEONG HWANG <octave08b@gmail.com>
2017-11-26 13:47:49 +0900

    implement RecycleView using adapter
    Activity에 RecyclerView 적용
    - DetailsBookAdapter를 이용하여  RecyclerView 적용

--- app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/BookDetailsActivity.kt
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/BookDetailsActivity.kt
@@ -1,8 +1,15 @@
 package com.wecon.lf_wannabe.walkonnovel.view
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.wecon.lf_wannabe.walkonnovel.BaseActivity
 import com.wecon.lf_wannabe.walkonnovel.R
+import com.wecon.lf_wannabe.walkonnovel.model.TestBook
+import com.wecon.lf_wannabe.walkonnovel.util.DummyFactory
+import com.wecon.lf_wannabe.walkonnovel.view.adapter.DetailsPostAdapter
+import com.wecon.lf_wannabe.walkonnovel.view.adapter.SpacesItemDecoration
+import kotlinx.android.synthetic.main.activity_book_details.*
 
 /**
  * Created by mangob on 2017. 11. 22..
@@ -17,5 +24,11 @@ class BookDetailsActivity : BaseActivity() {
         // toolbar
         setTitle("책 상세")
         setNavi()
+
+        // adapter
+        Log.i("Mangob/BookDetails", "Adapter")
+        bookDetailsRecyclerView.layoutManager = LinearLayoutManager(this)
+        bookDetailsRecyclerView.addItemDecoration(SpacesItemDecoration(10))
+        bookDetailsRecyclerView.adapter = DetailsPostAdapter(DummyFactory.createBookDummy())
     }
 }
