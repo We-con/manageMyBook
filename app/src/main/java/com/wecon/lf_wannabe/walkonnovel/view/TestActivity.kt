8857a3b2523c8d307c2d8dd1f5266c11472220de
GYUJEONG HWANG <octave08b@gmail.com>
2017-11-27 22:42:06 +0900

    Add GridView in activity
    GridView 추가
    - TestActivity에 GridView 추가
    - GridView에 BookGridAdapter 적용

--- app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/TestActivity.kt
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/TestActivity.kt
@@ -1,9 +1,14 @@
 package com.wecon.lf_wannabe.walkonnovel.view
 
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.wecon.lf_wannabe.walkonnovel.BaseActivity
 import com.wecon.lf_wannabe.walkonnovel.R
+import com.wecon.lf_wannabe.walkonnovel.view.adapter.BookGridAdpater
+import com.wecon.lf_wannabe.walkonnovel.viewmodel.BookViewModel
 import kotlinx.android.synthetic.main.activity_test.*
 
 /**
@@ -11,11 +16,21 @@ import kotlinx.android.synthetic.main.activity_test.*
  */
 class TestActivity : BaseActivity() {
 
+    private lateinit var viewModel: BookViewModel
+
+//    private var dp = this.resources.displayMetrics.density
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        // viewmodel
+        viewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
+
         // toolbar
         setTitle("Vook")
+
+        // GridView
+        testGridView.adapter = BookGridAdpater(viewModel.readAll())
     }
 }
