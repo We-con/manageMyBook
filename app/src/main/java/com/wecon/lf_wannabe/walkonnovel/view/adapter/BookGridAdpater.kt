24a8573c7a4f6af6a8c0b5fb1fc51ab96a3b2e84
GYUJEONG HWANG <octave08b@gmail.com>
2017-11-27 22:41:00 +0900

    Add BookGridAdpater
    BookGridAdpater 추가
    - Grid Item의 UI 추가 
    - Grid Item holder 추가
    - Grid view에 사용할 adapter 추가

---
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/adapter/BookGridAdpater.kt
@@ -0,0 +1,32 @@
+package com.wecon.lf_wannabe.walkonnovel.view.adapter
+
+import android.support.constraint.ConstraintLayout
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.wecon.lf_wannabe.walkonnovel.R
+import com.wecon.lf_wannabe.walkonnovel.model.Book
+import com.wecon.lf_wannabe.walkonnovel.view.holder.BookGridHolder
+
+/**
+ * Created by mangob on 2017. 11. 26..
+ */
+class BookGridAdpater(var books: ArrayList<Book>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var item = when(convertView){
+            null -> LayoutInflater.from(parent?.context)
+                    .inflate(R.layout.item_grid_book, parent, false) as ConstraintLayout
+            else -> convertView
+        }
+        BookGridHolder(item).bind(books[position])
+
+        return item
+    }
+
+    override fun getItem(position: Int): Book = books[position]
+    override fun getItemId(position: Int): Long = position.toLong()
+    override fun getCount(): Int = books.size
+
+}
