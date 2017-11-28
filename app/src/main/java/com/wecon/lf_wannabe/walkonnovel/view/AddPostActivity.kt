1a27953486e8e19f035992b60e852879f403ec09
CHOIMINSEOK <cms3718@gmail.com>
2017-11-28 11:28:07 +0900

    Fix the issue of Exiting PostAddActivity after Camera Activity

--- app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/AddPostActivity.kt
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/AddPostActivity.kt
@@ -130,7 +130,6 @@ class AddPostActivity: BaseActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode != Activity.RESULT_OK){
-            finish()
             return
         }
 
