8011c58e419cfa8542ed1d51d6f65a91c1685a74
GYUJEONG HWANG <octave08b@gmail.com>
2017-12-02 16:21:03 +0900

    Add EditText for search
    검색기능을 위한 EditText 추가 
    - toolbar에 EditText 추가
    - Search icon에 Clicklistener 추가

--- app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/MainActivity.kt
+++ app/src/main/java/com/wecon/lf_wannabe/walkonnovel/view/MainActivity.kt
@@ -6,12 +6,14 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.LinearLayout
 import com.wecon.lf_wannabe.walkonnovel.BaseActivity
 import com.wecon.lf_wannabe.walkonnovel.R
 import com.wecon.lf_wannabe.walkonnovel.view.adapter.BookGridAdpater
 import com.wecon.lf_wannabe.walkonnovel.viewmodel.BookViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_main.view.*
 
 /**
  * Created by mangob on 2017. 11. 26..
@@ -19,6 +21,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: BookViewModel
+    private var searchFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,23 @@ class MainActivity : AppCompatActivity() {
         mainGridView.setOnItemClickListener { parent, view, position, id ->
             var intent = Intent(this, BookDetailsActivity::class.java)
             startActivity(intent)
+        }
+
+        // Search
+        with(mainToolbar) {
+            toolbarImgAct.setOnClickListener {
+                when(searchFlag) {
+                    false -> {
+                        toolbarTextDesc.visibility = View.INVISIBLE
+                        toolbarEditSearch.visibility = View.VISIBLE
+                    }
+                    true -> {
+                        toolbarTextDesc.visibility = View.VISIBLE
+                        toolbarEditSearch.visibility = View.INVISIBLE
+                    }
+                }
+                searchFlag = !searchFlag
+            }
         }
     }
 }
